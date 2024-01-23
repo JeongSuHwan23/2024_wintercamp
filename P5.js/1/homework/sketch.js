b2389da3b1644e9e9331564912f043c9feb8e310
JungSuHwan23 <short9666@gmail.com>
2024-01-23 10:55:00 +0900

    2일차

--- P5.js/1/homework/sketch.js
+++ P5.js/1/homework/sketch.js
@@ -1,31 +1,55 @@
 let snake;
+let food;
+let rez = 20;
+let w;
+let h;
 
 function setup() {
   createCanvas(400, 400);
+  frameRate(7);
   snake = new Snake();
+  w = floor(width/rez);
+  h = floor(height/rez)
+  locateFood();
 }
 
-function keyPressed(){
-  if(keyCode == LEFT_ARROW){
-    snake.xdir=-1;
-    snake.ydir=0;
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode == RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode == UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (keyCode == DOWN_ARROW) {
+    snake.setDir(0, 1);
   }
-  else if(keyCode == RIGHT_ARROW){
-    snake.xdir=1;
-    snake.ydir=0;
-  }
-  else if(keyCode == UP_ARROW){
-    snake.xdir=0;
-    snake.ydir=-1;
-  }
-  else if(keyCode == DOWN_ARROW){
-    snake.xdir=0;
-    snake.ydir=1;
-  }
+}
+
+function locateFood(){
+  food = createVector(floor(random(w)), floor(random(h)));
 }
 
 function draw() {
   background(220);
+  scale(rez);
+  
+  //grow snake
+  if(snake.eat(food)){
+    locateFood();
+  }
+  
+  // if(snake.endGame()){
+  //   console.log("End Game");
+  //   background(255,0,0);
+  //   noLoop();
+  // }
+  
+  //draw snake
   snake.update();
   snake.show();
+  
+  //draw food
+  fill(255,0,0);
+  noStroke();
+  rect(food.x, food.y, 1, 1);
 }
--- P5.js/1/homework/snake.js
+++ P5.js/1/homework/snake.js
@@ -5,20 +5,51 @@ class Snake {
     this.xdir = 0;
     this.ydir = 0;
   }
-  
-  update() {
-    this.body[0].x += this.xdir;
-    this.body[0].y += this.ydir;
+
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  eat(food) {
+    if (this.body[this.body.length - 1].x === food.x && this.body[this.body.length - 1].y === food.y){
+      this.grow();  
+      return true;
+  }
+    else
+      return false;
+  }
+
+  grow() {
+    let head = this.body[this.body.length - 1].copy()
+    this.body.push(head);
   }
   
+//   endGame(){
+//     //if (뱀의 머리가 경계선을 초과)
+//     //if (뱀의 머리가 몸통에 닿음)
+    
+//     return false;
+//   }
+  
+  update() {
+    let head = this.body[this.body.length - 1].copy()
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+
   updated() {
     this.body[0].x -= this.xdir;
     this.body[0].y -= this.ydir;
   }
-  
+
   show() {
-    fill(0);
-    noStroke();
-    rect(this.body[0].x,this.body[0].y,10,10);
+    for(let i=0; i<this.body.length; i++){
+      fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
   }
 }
